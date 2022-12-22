27bae1d365e043c02872b4a6034e88b92ef4b236
jppgomez <77557946+jppgomez@users.noreply.github.com>
2022-12-22 16:09:48 +0000

    education section design and implementation

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+  
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
         document.querySelector(
@@ -16,10 +20,12 @@ document.onreadystatechange = function() {
     }
 };
 
-
 function design_skills_bar(n){
     let bar = document.querySelector(".design_skills_bar");
     let selector = document.querySelector(".design_skills_image_" + n);
+    for(let a=0; a<7; a++){
+        document.querySelector(".design_skills_image_" + a).classList.add("hide");
+    }
     selector.classList.remove("hide");
     if(n==0 && bar.style.width != '70%'){
         bar.style.width = '70%';
@@ -54,6 +60,9 @@ function design_skills_bar(n){
 function prog_skills_bar(n){
     let bar = document.querySelector(".prog_skills_bar");
     let selector = document.querySelector(".prog_skills_image_" + n);
+    for(let a=0; a<5; a++){
+        document.querySelector(".prog_skills_image_" + a).classList.add("hide");
+    }
     selector.classList.remove("hide");
 
     if(n==0 && bar.style.width != '75%'){
@@ -73,9 +82,39 @@ function prog_skills_bar(n){
     }
     else {
         for(let a=0; a<5; a++){
-            console.log(document.querySelector(".prog_skills_image_" + a));
             document.querySelector(".prog_skills_image_" + a).classList.add("hide");
         }
         bar.style.width = '0%';
     }
     }
+
+    function education_bar(n){
+        let bar = document.querySelector(".education_bar");
+        let selector = document.querySelector(".education_image_" + n);
+        for(let a=0; a<3; a++){
+            document.querySelector(".education_image_" + a).classList.add("hide");
+        }
+        selector.classList.remove("hide");
+    
+        let text = document.querySelector(".education_text");
+        text.innerText = "";
+        if(n==0 && bar.style.width != '2%'){
+            bar.style.width = '2%';
+            text.innerText = "Ciências e Tecnologias na Escola Secundária José Loureiro Botas";
+        }
+        else if(n==1 && bar.style.width != '35%'){
+            bar.style.width = '35%';
+            text.innerText = "Licenciatura em Design e Multimédia na Faculdade de Ciências e Tecnologia da Universidade de Coimbra";
+        }
+        else if(n==2 && bar.style.width != '70%'){
+            bar.style.width = '70%';
+            text.innerText = "Mestrado em Design e Multimédia na Faculdade de Ciências e Tecnologia da Universidade de Coimbra";
+        }
+        else {
+            for(let a=0; a<3; a++){
+                document.querySelector(".education_image_" + a).classList.add("hide");
+            }
+            bar.style.width = '0%';
+        }
+
+        }
