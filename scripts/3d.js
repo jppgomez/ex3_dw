2f1fe2d1bf93b5ca1d88841de619f4bfbb3903ed
jppgomez <77557946+jppgomez@users.noreply.github.com>
2022-12-22 12:36:38 +0000

    error page design and implementation

--- scripts/3d.js
+++ scripts/3d.js
@@ -27,5 +27,3 @@ const canvas_about_img = document.getElementById('canvas3d_about_images');
 const app_about_img = new Application(canvas_about_img);
 app_about_img.load('https://prod.spline.design/ctNRQ7nCRBhxLfkm/scene.splinecode');
 
-//404
-//https://prod.spline.design/oknR1dk4sQQvhD1S/scene.splinecode
